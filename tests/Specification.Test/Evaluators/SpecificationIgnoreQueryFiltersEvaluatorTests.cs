a4430c257554e8f0c5cb14b3a54d85c3eed7b3a0
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2025-07-03 13:59:06 -0600

    Add unit tests for query filter and split query features
    Introduced two new test classes: `SpecificationIgnoreQueryFiltersEvaluatorTests` and `SpecificationSplitQueryEvaluatorTests`. Each class includes tests to verify the correct application and non-application of query filter and split query functionalities in the `Specification` class based on their enabled or disabled states.

---
+++ tests/Specification.Test/Evaluators/SpecificationIgnoreQueryFiltersEvaluatorTests.cs
@@ -0,0 +1,35 @@
+﻿using API;
+using Specification.Lite;
+
+namespace Specification.Test.Evaluators;
+
+public class SpecificationIgnoreQueryFiltersEvaluatorTests
+{
+    [Fact]
+    public void IgnoreQueryFilters_ShouldApplyIgnoreQueryFilters_WhenEnabled()
+    {
+        // Arrange
+        var specification = new Specification<TestEntity>();
+        var specificationResult = new Specification<TestEntity, string>();
+
+        // Act
+        specification.Query.IgnoreQueryFilters();
+        specificationResult.Query.IgnoreQueryFilters();
+
+
+        // Assert
+        Assert.True(specification.IgnoreQueryFilters);
+        Assert.True(specificationResult.IgnoreQueryFilters);
+    }
+
+    [Fact]
+    public void IgnoreQueryFilters_ShouldNotApplyIgnoreQueryFilters_WhenDisabled()
+    {
+        var specification = new Specification<TestEntity>();
+        var specificationResult = new Specification<TestEntity, string>();
+
+        // Act & Assert
+        Assert.False(specification.IgnoreQueryFilters);
+        Assert.False(specificationResult.IgnoreQueryFilters);
+    }
+}
---
+++ tests/Specification.Test/Evaluators/SpecificationSplitQueryEvaluatorTests.cs
@@ -0,0 +1,35 @@
+﻿using API;
+using Specification.Lite;
+
+namespace Specification.Test.Evaluators;
+
+public class SpecificationSplitQueryEvaluatorTests
+{
+    [Fact]
+    public void SplitQuery_ShouldApplySplitQuery_WhenEnabled()
+    {
+        // Arrange
+        var specification = new Specification<TestEntity>();
+        var specificationResult = new Specification<TestEntity, string>();
+
+        // Act
+        specification.Query.SplitQuery();
+        specificationResult.Query.SplitQuery();
+
+
+        // Assert
+        Assert.True(specification.AsSplitQuery);
+        Assert.True(specificationResult.AsSplitQuery);
+    }
+
+    [Fact]
+    public void SplitQuery_ShouldNotApplySplitQuery_WhenDisabled()
+    {
+        var specification = new Specification<TestEntity>();
+        var specificationResult = new Specification<TestEntity, string>();
+
+        // Act & Assert
+        Assert.False(specification.AsNoTracking);
+        Assert.False(specificationResult.AsSplitQuery);
+    }
+}
